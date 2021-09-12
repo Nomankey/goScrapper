e65c5502fdb73f45303133feb14069d3710f5486
Shawn CK.P <snowy213@outlook.com>
2021-09-12 15:15:12 -0700

    first go function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## What is GO?
+
+
+### what is fmt package
+it's a package for formatting
+```
+fmt.Println("Hello world") //fmt.print line
+```
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main //compile project
 
-import "fmt";
+import (
+	"fmt"
+)
+
+func multiply(a int, b int) int {
+	return a * b
+}
 
 func main() {
-fmt.Println("Hello World")
+	fmt.Println(multiply(2, 2))
 }
