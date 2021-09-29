1cdd5419847a4516799254f0d0e5a585c2177127
Shawn CK.P <snowy213@outlook.com>
2021-09-29 16:42:09 -0700

    finished

--- main.go
+++ main.go
@@ -2,20 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/nomankey/learngo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nomankey")
-
 	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		log.Println(err)
-	}
-	account.Withdraw(5)
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
